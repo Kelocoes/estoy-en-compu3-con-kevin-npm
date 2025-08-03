900605f1ee0d90141acf7ec3abe3b7f7f8f8fd38
kelocoes <kevin.rodriguez109@gmail.com>
2025-08-02 21:39:50 -0500

    Tesing things~!

---
+++ index.js
@@ -0,0 +1,5 @@
+function estoyEnCompu3ConKevin(isHeInPajamas) {
+    return isHeInPajamas ? "Sí, estás en Compunet 3 con Kevin." : "No, no estás en Compunet 3 con Kevin.";
+}
+
+module.exports = estoyEnCompu3ConKevin
